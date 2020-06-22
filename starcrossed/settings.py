eca75e5f7773d87087847aedb0a333f684b3ab40
drakyl0n <brianliu9606@gmail.com>
2020-06-21 20:43:56 -0700

    stub up profile model

--- starcrossed/settings.py
+++ starcrossed/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'starcrossed',
-        'USER': 'postgres',
-        'PASSWORD': 'Lunatari7',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'Lunatari7',
     }
 }
 
