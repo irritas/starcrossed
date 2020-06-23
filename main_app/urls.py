bdf539c42588cb4482956aaf5fefc431668eea9c
drakyl0n <brianliu9606@gmail.com>
2020-06-23 16:54:02 -0700

    finish landing and sign detail pages

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
-    path('accounts/index/', views.user_index),
-    path('signs/<sign>/', views.signs_detail, name='detail'),
+    # path('accounts/index/', views.user_index),
+    path('signs/', views.signs_index, name='signs_index'),
+    path('signs/<sign_name>/', views.signs_detail, name='signs_detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,10 +4,10 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-import requests
+import requests, json
 
 def home(request):
-    return render(request, 'home.html')
+    return redirect('signs_index')
 
 def signup(request):
     error_message = ''
@@ -23,13 +23,38 @@ def signup(request):
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
 
-@login_required
-def user_index(request):
-    users = User.objects.all()
-    return render(request, 'user/index.html', { 'users': users })
+# @login_required
+# def user_index(request):
+#     users = User.objects.all()
+#     return render(request, 'user/index.html', { 'users': users })
 
-def signs_detail(request, sign):
-	fortune = requests.get(f'http://horoscope-api.herokuapp.com/horoscope/today/{sign}').json()
+SIGNS_LOOKUP = {
+	'Aries': 'Ram',
+	'Taurus': 'Bull',
+	'Gemini': 'Twins',
+	'Cancer': 'Crab',
+	'Leo': 'Lion',
+	'Virgo': 'Maiden',
+	'Libra': 'Scales',
+	'Scorpio': 'Scorpion',
+	'Sagittarius': 'Archer',
+	'Capricorn': 'Goat',
+	'Aquarius': 'Water-bearer',
+	'Pisces': 'Fish'
+}
+
+def signs_index(request):
+	return render(request, 'signs/index.html', {
+		'signs': SIGNS_LOOKUP
+	})
+
+def signs_detail(request, sign_name):
+	sign = requests.get(f'https://zodiacal.herokuapp.com/{sign_name}').json()[0]
+	fortune = requests.get(f'https://horoscope-api.herokuapp.com/horoscope/today/{sign_name}').json()
+	symbol = SIGNS_LOOKUP[sign_name.capitalize()]
 	return render(request, 'signs/detail.html',	{
-		'sign': sign
+		'sign': sign,
+		'symbol': symbol,
+		'fortune': fortune,
+		'img_url': f'images/{sign_name}.png'
 	})
