12c1519f79fb74bb60a86b11508776651e1d3bce
Charlotte Pak <charlottepak@outlook.com>
2020-06-24 15:23:41 -0700

    add styling

--- main_app/models.py
+++ main_app/models.py
@@ -21,3 +21,12 @@ class Profile(models.Model):
 #   name = models.CharField(max_length=100)
 #   birthdate = models.DateField()
 #   bio = models.TextField(max_length=250)
+
+
+#Sarah's code
+# class Profile(models.Model):
+#     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     causes = models.ManyToManyField(Cause)
+#     purchased_items = models.ManyToManyField(Order)
+#     def __str__(self):
+#         return self.user
