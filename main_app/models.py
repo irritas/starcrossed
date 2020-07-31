a67a46b92ab4ed3619072a82016acfb3c8af7ed3
drakyl0n <brianliu9606@gmail.com>
2020-07-30 22:06:57 -0700

    format tabbing

--- main_app/models.py
+++ main_app/models.py
@@ -13,11 +13,11 @@ class Profile(models.Model):
     image = models.ImageField(upload_to='profile_image', blank=True)
 
 class Photo(models.Model):
-        url = models.CharField(max_length=200)
-        user = models.ForeignKey(User, on_delete=models.CASCADE)
+	url = models.CharField(max_length=200)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-        def __str__(self):
-            return f"Photo for user_id: {self.user_id} @{self.url}"
+	def __str__(self):
+		return f"Photo for user_id: {self.user_id} @{self.url}"
 
 @receiver(post_save, sender=User)
 def update_user_profile(sender, instance, created, **kwargs):
--- main_app/urls.py
+++ main_app/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('accounts/signup/', views.signup, name='signup'),
-    path('users/', views.users_index, name='users_index'),
+	path('', views.home, name='home'),
+	path('accounts/signup/', views.signup, name='signup'),
+	path('users/', views.users_index, name='users_index'),
 	path('users/<int:user_id>/', views.users_detail, name='users_detail'),
-    path('signs/', views.signs_index, name='signs_index'),
-    path('signs/<sign_name>/', views.signs_detail, name='signs_detail'),
-    path('users/<int:user_id>/add_photo/', views.add_photo, name='add_photo'),
-    path('chats/add_chat/<user_a_id>+<user_b_id>/', views.add_chat, name='add_chat'),
+	path('signs/', views.signs_index, name='signs_index'),
+	path('signs/<sign_name>/', views.signs_detail, name='signs_detail'),
+	path('users/<int:user_id>/add_photo/', views.add_photo, name='add_photo'),
+	path('chats/add_chat/<user_a_id>+<user_b_id>/', views.add_chat, name='add_chat'),
 	path('chats/<int:chat_id>/', views.chats_detail, name='chats_detail'),
 	path('chats/<int:pk>/delete/', views.ChatDelete.as_view(), name='chats_delete'),
 	path('users/<int:user_id>/update/', views.bio_update, name='bio_update'),
